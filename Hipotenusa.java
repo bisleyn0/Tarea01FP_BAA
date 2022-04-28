c7f4a4c89ee1ca57244b07cfca794d9b07873b83
bisleyn0 <bisleyn0@gmail.com>
2022-04-28 09:08:26 -0500

    mi primer trabajo

---
+++ Hipotenusa.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Hipotenusa {
+
+    public static void main(String[] args) {
+        //Definir variables
+        double a, b, c;
+        //Datos de entrada
+        Scanner lt=new Scanner(System.in);
+        System.out.println("ingrese el lado a:");
+        a=lt.nextDouble();
+        System.out.println("ingrese el lado b:");
+        b=lt.next
+        
+    }() {
+        
+    }
+}
+
+    
+}
